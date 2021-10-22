528465c251759005efe66043410af4737a40cbc3
Davidpavan <David@zelarsoft.com>
2021-10-22 19:37:38 +0530

    C

--- basics.ts
+++ basics.ts
@@ -12,50 +12,77 @@ isAdmin = false;
 let a = 10;
 // a = "hello";
 // a = 20;
-// a = { name: "Venkatesh" };
+// a = { name: "pavan" };
 // a = [1, 2, 3];
 // a = 10;
 // a = "sdlfk";
-// a = { name: "vivek" };
+// a = { name: "pavan" };
 // for (let i = 0; i < a.length; i++) {
 //     console.log(a[i]);
 // }
 //5.Arrays
 let students = [1, 2, 3];
 students = [10];
-let teachers = ["vivek"];
-teachers = ["venkatesh"];
+let teachers = ["pavan"];
+teachers = ["david"];
 let users = [];
 users = [10, "sadf"];
 //6.Tuples
-let user = ["venkatesh", 20];
-user = ["Vivek", 10];
-let userData = [];
-userData = [["vivek", 20], ["venkatesh", 10]];
-//7.Union
-let person;
-person = "Venkatesh";
-person = 20;
+//Tuple --> with tuple you can specify the exact types inside array
+let person1: [number, string, boolean] = [1, 'ben', true] // ['ben', true,1] --> it gives error because you'veto give it order as you mentioned
+
+//Tuple array
+let employee: [number, string] [] // [] for array
+
+employee = [    // we can write array inside array
+    [1, 'ben'],
+    [2, 'john'],
+    [3, 'david'],
+]
+
+//Union
+//--> if you want to a particulaar variable to be hold more than one type you can do that with union.
+
+let pid: string | number
+
+pid = 22 //as number  or pid = '22' as string also it would work.
+pid = "pavan"
 //8.Enum
 const RED = "Red";
 const BLUE = "Blue";
-var Colors;
-(function (Colors) {
-    Colors["RED"] = "Red";
-    Colors["BLUE"] = "Blue";
-    Colors["GREEN"] = "Green";
-})(Colors || (Colors = {}));
-console.log(Colors.RED);
-let car = {
-    name: "Telsa",
-    price: 100
-};
-car.name = "Audi";
+enum Colors{
+    RED = "red",
+    BLUE = "blue",
+    GREEN = "green"
+}
+console.log(Colors.RED)
+
+//9.Objects
+
+// Object is an instance which contains set of key value pairs
+// const user = {
+//     id: 1,
+//     name: 'ben'
+// }
+// if u want to add types to above values we can do that
+type User ={
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'ben'
+}
+
 //10.Type Assertion
-let username = "Vivek";
-let userId = username;
-let userNames = username;
-let firstname = username;
+let username: unknown = "Vivek";
+let userId: number = <number>username;
+let userNames: string = <string>username;
+
+// let customerId = <number>cid  we cn do this other way
+let customerId = username as number
+
 //11.null, undefined
-let abc = null;
-let noValue = undefined;
+let abc: null = null;
+let noValue: undefined = undefined;
